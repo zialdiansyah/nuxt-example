49e3ee15219ff1ce788d0cb1559e74215865bcf1
zialdiansyah <ziaulhafizda@gmail.com>
2020-12-09 10:43:07 +0000

    add new example page

---
+++ pages/example.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container mx-auto flex flex-col justify-center h-screen">
+    <div class="self-center">
+      <h1 class="text-3xl text-center mb-2">
+        Example
+      </h1>
+      <p class="text-large text center mb-5">Boilerplate to start you nuxt-tailwind project</p>
+      <nuxt-link
+        class="bg-transparent hover:bg-gray-800 text-gray-800 hover:text-white border border-gray-800 font-semibold p-4 rounded"
+        to="/yugioh"
+      >
+        Card
+      </nuxt-link>
+      <nuxt-link
+        class="bg-transparent hover:bg-gray-800 text-gray-800 hover:text-white border border-gray-800 font-semibold p-4 rounded"
+        to="/epl"
+      >
+        Table
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
