1bbccb4f2675dae2788405a6883390a50733abd8
zialdiansyah <ziaulhafizda@gmail.com>
2020-12-09 10:43:43 +0000

    add about text

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container mx-auto flex flex-col justify-center h-screen">
     <div class="self-center">
-      <h1 class="text-3xl text-center mb-5">
+      <h1 class="text-3xl text-center mb-2">
         About
       </h1>
+      <p class="text-large text center mb-5">Boilerplate to start you nuxt-tailwind project</p>
       <nuxt-link
         class="bg-transparent hover:bg-gray-800 text-gray-800 hover:text-white border border-gray-800 font-semibold p-4 rounded"
         to="/"
